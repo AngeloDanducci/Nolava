04ebe0a6df4543af6eb5802482e0edef460adcc5
beefcheese <christysteven@gmail.com>
2016-03-22 14:20:00 -0400

    Updated a bit. Provided better support for reconnecting and what not.                                Changed a bit on how roles are assigned, roles are now assigned when                                the game starts instead of when they choose a place on the board. The                                First quest leader is spot #1, I suppose we go in order numerically.

--- app/nolava.py
+++ app/nolava.py
@@ -10,10 +10,10 @@ from user import User
 # empty string here for all available interfaces, same as socket
 ws = WSocket('', 1400, 0)
 roles = ['nilrem', 'nissassa', 'derdrom', 'lavicrep', 'anagrom', 'good', 'evil']
-rolesLeft = list(roles)
 stateList = ['not_started', 'choose_team', 'vote_quest', 'quest_success_or_fail', 'assassinate']
 state = 'not_started'
 users = []
+disconnected = []
 
 def htmlEncode(text):
 	return html.escape(text)
@@ -23,7 +23,7 @@ def initUser(user):
 	# WSocket.send(user.socket, "request:name")
 	pass
 
-def play_as(user, place):
+def playAs(user, place):
 	# Where place is 1-10 on the board
 	for u in users:
 		if u.place == place:
@@ -32,18 +32,65 @@ def play_as(user, place):
 	user.place = place
 	user.session = uuid.uuid1()
 	# How serious a session token do we need?... http://stackoverflow.com/a/6092448/1450120
-	user.role = rolesLeft[random.randrange(0, len(rolesLeft))]
-	if user.role != 'good' and user.role != 'evil':
-		rolesLeft.remove(user.role)
-	WSocket.send(user.socket, 'assign,%s:%s' % (user.place, user.role))
-	WSocket.send(user.socket, 'session:%s' % (user.session))
-	whoami(user)
+	send(user, 'assign:%s' % (user.place))
+	send(user, 'session:%s' % (user.session))
 
 def whoami(user):
-	# Tell the user everything they should know based on what role they are
+	# Tell the user everything they should know based on what role they are (including score etc.)
 	# This function should be used in two places, when a user reconnects, and when they
 	# are first assigned a role
 	pass
+
+def send(user, data):
+	try:
+		WSocket.send(user.socket, data)
+	except:
+		# If we couldn't send data to a user, remove them from list of users,
+		# add to disconnected list, and notify everyone else the user disconnected
+		try:
+			users.remove(user)
+			# Save the user if they have a session (which means they are playing)
+			if user.session != None:
+				disconnected.append(user)
+			for u in users:
+				send(u, '%s:%s' % ('disconnected', user.name))
+		except ValueError:
+			# If the user is not users trying to remove it creates a value error.
+			# catching the error is useful here to not spam users about a disconnect
+			# and prevent unintended loops
+			pass
+
+def startGame():
+	# TODO: assign users a role, pick first team leader
+	# all users that have an assigned place
+	usersPlaying = [x for x in users if x.place != None]
+	random.shuffle(usersPlaying)
+	# IMPORTANT! Shuffle users otherwise players that connected earlier are much more likely to
+	# be assigned good/evil rather than a unique character
+	rolesLeft = list(roles)
+	rolesLeft.remove('evil')
+	rolesLeft.remove('good')
+
+	while len(usersPlaying) > 0:
+		if len(usersPlaying) > len(rolesLeft):
+			# if there are more players left than unique roles, assign
+			# randomly either a unique role or good/evil
+			if random.getrandbits(1):
+				role = random.choice(rolesLeft)
+				rolesLeft.remove(role)
+			else:
+				role = random.choice(['evil', 'good'])
+		else:
+			role = random.choice(rolesLeft)
+			rolesLeft.remove(role)
+
+		user = usersPlaying.pop(0)
+		send(user, 'assign:%s' % (role))
+		if user.place == 1:
+			send(user, 'quest:leader')
+			for u in users:
+				send(u, 'leader:%s' % (user.place))
+
 
 userId = 1
 while True:
@@ -72,22 +119,37 @@ while True:
 
 		if action[0] == 'chat':
 			for u in users:
-				WSocket.send(u.socket, "chat:%s:%s" % (htmlEncode(user.name), htmlEncode(action[1])))
-		elif action[0] == 'vote':
-			# Should be either reject/approve or success/fail depending on what vote is for
-			pass
+				send(u, "chat:%s:%s" % (htmlEncode(user.name), htmlEncode(action[1])))
 		elif action[0] == 'name':
 			# When someone updates their name, tell everyone
 			# Could potentially be abused? TODO: rate limit? only allow once?
 			# Check for duplicate name?
 			user.name = action[1]
 			for u in users:
-				WSocket.send(u.socket, '%s:%s' % (user.id, htmlEncode(user.name)))
+				send(u, '%s:%s' % (user.id, htmlEncode(user.name)))
+			pass
+		elif action[0] == 'session':
+			# If user is trying to reconnect, make sure they are given the correct user object
+			sessionId = action[1]
+			exists = False
+			for u in disconnected:
+				if u.session == sessionId:
+					exists = True
+					users.remove(user)
+					users.append(u)
+					disconnected.remove(u)
+					u.socket = user.socket
+			if exists:
+				print('Tracked user reconnected.')
+			else:
+				print('Untracked user connected with sessionId = %s' % (sessionId))
+		elif action[0] == 'vote':
+			# Should be either reject/approve or success/fail depending on what vote is for
 			pass
 		elif action[0] == 'join':
 			# User will ask to join as id# (1-10) which corresponds to their position in the board
 			# Make sure to check if the id is taken...
-			play_as(user, action[1])
+			playAs(user, action[1])
 		elif action[0] == 'whoami':
 			# If the client reconnects, the browser won't "remember" who is who
 			# so remind them
