89139dd0f0f4cc4cefdfdc39fcb7a7009735bbd9
beefcheese <christysteven@gmail.com>
2016-04-08 18:36:23 -0400

    Couple changes - can't remember - forgot to commit ^^Best commit ever

--- app/nolava.py
+++ app/nolava.py
@@ -4,12 +4,13 @@ import time
 import random
 import uuid
 import html
+import re
 from wsocket import WSocket
 from user import User
 
 # empty string here for all available interfaces, same as socket, 0 for timeout
 ws = WSocket('', 1400, 0)
-roles = ['nilrem', 'nissassa', 'derdrom', 'lavicrep', 'anagrom', 'good', 'evil']
+allRoles = ['nilrem', 'nissassa', 'derdrom', 'lavicrep', 'anagrom', 'good', 'evil']
 stateList = ['not_started', 'choose_team', 'vote_quest', 'quest_success_or_fail', 'assassinate']
 state = 'not_started'
 timerStart = None # DateTime
@@ -17,6 +18,8 @@ timerLength = 60 # seconds
 leaderPlace = 0
 users = []
 disconnected = []
+sessions = {}
+roles = {'nilrem':None, 'nissassa':None, 'derdrom':None, 'lavicrep':None, 'anagrom':None, 'good':[], 'evil':[]}
 
 def htmlEncode(text):
 	return html.escape(text)
@@ -28,6 +31,10 @@ def initUser(user):
 
 def playAs(user, place):
 	# Where place is 1-10 on the board
+	global sessions
+	if user.place != None:
+		# User already has a position
+		send(user, 'error:%s %s' % ('You are already place in position', user.place))
 	if state == 'not_started':
 		for u in users:
 			if u.place == place:
@@ -39,6 +46,7 @@ def playAs(user, place):
 		# How serious a session token do we need?... http://stackoverflow.com/a/6092448/1450120
 		send(user, 'assign:%s' % (user.place))
 		send(user, 'session:%s' % (user.session))
+		sessions[user.session] = user
 	else:
 		send(user, 'error:%s' % ('Sorry, the game has started'))
 
@@ -46,11 +54,62 @@ def whoami(user):
 	# Tell the user everything they should know based on what role they are (including score etc.)
 	# This function should be used in two places, when a user reconnects, and when they
 	# are first assigned a role
-	pass
+	print('whoami')
+	send(user, 'state:%s' % (state))
+
+	send(user, 'assign:%s' % (user.place))
+	send(user, 'assign:%s' % (user.role))
+
+	if user.role == 'nilrem':
+		# Sees all evil players except derdrom
+		notifyEvilPlayers(user, False)
+	elif user.role == 'lavicrep':
+		# Knows who Merlin is, (but Morgana is also shown as Merlin)
+		# use queue of messages to randomize the order
+		queue = []
+		if roles['anagrom'] != None:
+			queue.append('place:%s:%s:%s' % (roles['anagrom'].place, roles['anagrom'].name, 'nilrem'))
+		if roles['nilrem'] != None:
+			queue.append('place:%s:%s:%s' % (roles['anagrom'].place, roles['anagrom'].name, 'nilrem'))
+
+		random.shuffle(queue)
+		for msg in queue:
+			send(user, msg)
+	elif user.role == 'derdrom' or user.role == 'anagrom' or user in roles['evil']:
+		notifyEvilPlayers(user, True)
+
+	# Finally, notify the user of all spots that are taken... Useful if the game is still joinable and whatnot
+	for u in [x for x in users if x.place != None]:
+		send(user, 'place:%s:%s' % (u.place, u.name))
+
+	# TODO also show quest leader and score!
+
+
+def notifyEvilPlayers(user, includeDerdrom):
+	# In order to prevent the client from being like "Hey, the guy who's sent last is the assassin"
+	# queue the messages in a list and randomize them...
+	queue = []
+	for u in roles['evil']:
+		queue.append('place:%s:%s:%s' % (u.place, u.name, u.role))
+
+	if includeDerdrom and roles['derdrom'] != None:
+		queue.append('place:%s:%s:%s' % (roles['derdrom'].place, roles['derdrom'].name, 'derdrom'))
+
+	if roles['anagrom'] != None:
+		queue.append('place:%s:%s:%s' % (roles['anagrom'].place, roles['anagrom'].name, 'anagrom'))
+
+	if roles['nissassa'] != None:
+		queue.append('place:%s:%s:%s' % (roles['nissassa'].place, roles['nissassa'].name, 'nissassa'))
+
+	random.shuffle(queue)	
+	for msg in queue:
+		send(user, msg)
+
 
 def send(user, data):
 	try:
 		WSocket.send(user.socket, data)
+		return True
 	except:
 		# If we couldn't send data to a user, remove them from list of users,
 		# add to disconnected list, and notify everyone else the user disconnected
@@ -61,20 +120,23 @@ def send(user, data):
 				disconnected.append(user)
 			for u in users:
 				send(u, '%s:%s' % ('disconnected', user.name))
+
+			return False
 		except ValueError:
 			# If the user is not users trying to remove it creates a value error.
 			# catching the error is useful here to not spam users about a disconnect
 			# and prevent unintended loops
-			pass
+			return False
 
 def startGame():
 	# Give all users that have an assigned place a role and start the first round
 	# all users that have an assigned place
+	global roles
 	usersPlaying = [x for x in users if x.place != None]
 	random.shuffle(usersPlaying)
 	# IMPORTANT! Shuffle users otherwise players that connected earlier are much more likely to
 	# be assigned good/evil rather than a unique character
-	rolesLeft = list(roles)
+	rolesLeft = list(allRoles)
 	rolesLeft.remove('evil')
 	rolesLeft.remove('good')
 
@@ -92,10 +154,22 @@ def startGame():
 			rolesLeft.remove(role)
 
 		user = usersPlaying.pop(0)
+		user.role = role
 		send(user, 'assign:%s' % (role))
-		whoami(user)
+		if role == 'evil' or role == 'good':
+			roles[role].append(user)
+		else:
+			roles[role] = user
 
 	startRound()
+	for user in users:
+		whoami(user)
+
+def numOrInf(x):
+	if not x:
+		return 99999999999
+	else:
+		return x
 
 def startRound():
 	# Assign new leader, start timer
@@ -108,13 +182,14 @@ def startRound():
 
 	state = 'choose_team'
 	leaderPlace = leaderPlace + 1
-	newLeader = next(x for x in users if x.place == leaderPlace)
+	#newLeader = next(x for x in users if x.place == leaderPlace)
+	newLeader = min(users, key=lambda x: numOrInf(x.place))
 
 	newLeader.teamLeader = True
 	send(newLeader, 'quest:leader')
 
 	for user in users:
-		send(user, 'leader:%s' % (user.place))
+		send(user, 'leader:%s' % (leaderPlace))
 
 	startTimer()
 
@@ -187,25 +262,24 @@ try:
 				# When someone updates their name, tell everyone
 				# Could potentially be abused? TODO: rate limit? only allow once?
 				# Check for duplicate name?
-				user.name = action[1]
-				for u in users:
-					send(u, '%s:%s' % (user.id, htmlEncode(user.name)))
+				regex = r'(\w+).*'
+				match = re.match(regex, action[1])
+				if match:
+					user.name = match.group(1)
+					for u in users:
+						send(u, '%s:%s' % (user.id, htmlEncode(user.name)))
 				pass
 			elif action[0] == 'game':
 				gameAction(action[1])
 			elif action[0] == 'session':
 				# If user is trying to reconnect, make sure they are given the correct user object
 				sessionId = action[1]
-				exists = False
-				for u in disconnected:
-					if u.session == sessionId:
-						exists = True
-						users.remove(user)
-						users.append(u)
-						disconnected.remove(u)
-						u.socket = user.socket
-				if exists:
+				if sessionId in sessions:
 					print('Tracked user reconnected.')
+					sessions[sessionId] = user
+					if user in disconnected:
+						disconnected.remove(user)
+						users.append(user)
 				else:
 					print('Untracked user connected with sessionId = %s' % (sessionId))
 			elif action[0] == 'vote':
